fa70dc3ec632dca1f212510bdc6c59c80d630af5
Yehor Hryhortsev <egor.goldenrash@gmail.com>
2024-01-10 17:41:36 +0200

    add routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,23 @@
+import Layout from 'components/Layout/Layout';
+import DashboardPage from 'pages/DashboardPage';
+import LoginPage from 'pages/LoginPage';
+import RegistrationPage from 'pages/RegistrationPage';
+import { Route, Routes } from 'react-router';
 import Global from 'styles/global';
 
 export const App = () => {
   return (
     <>
       <Global />
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<DashboardPage />}>
+            <Route path="/statistics" element={<RegistrationPage />} />
+          </Route>
+          <Route path="/register" element={<RegistrationPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Route>
+      </Routes>
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App/App';
 import './index.css';
+import { Provider } from 'react-redux';
+import { store } from 'store/store';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="project-codefusion">
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
