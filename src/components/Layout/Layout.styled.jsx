85f40dccd468e0274e602d65119de7057b92c3af
Yehor Hryhortsev <egor.goldenrash@gmail.com>
2024-01-11 20:33:31 +0200

    add styles

--- src/components/Layout/Layout.styled.jsx
+++ src/components/Layout/Layout.styled.jsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   /* containers */
-
+  position: relative;
   min-width: 320px;
   max-width: 375px;
   margin: 0 auto;
@@ -16,7 +16,14 @@ export const Container = styled.div`
   }
 
   @media only screen and (min-width: 1272px) {
-    max-width: 1272px;
+    max-width: 1312px;
     padding: 0 16px;
   }
+`;
+
+export const WrapperLeftAndRight = styled.div`
+  display: flex;
+`;
+export const WrapperLeft = styled.div`
+  border-right: 1px solid rgba(255, 255, 255, 0.6);
 `;
