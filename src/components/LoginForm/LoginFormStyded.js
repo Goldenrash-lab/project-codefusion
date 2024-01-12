4f0264972fff5cb338ec048dde5de4483b340f29
DmytroPrymachenko <144675672+DmytroPrymachenko@users.noreply.github.com>
2024-01-12 14:13:17 +0200

    фікс

--- src/components/LoginForm/LoginFormStyded.js
+++ src/components/LoginForm/LoginFormStyded.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 import backgroundImageImac from '../../images/Login/LoginFonIMac.jpg';
-
+import backgroundImageTablet from '../../images/Login/LoginFonTablet.jpg';
+import backgroundImageMobail from '../../images/Login/LoginGonMobile.jpg';
 export const LoginDiv = styled.div`
   width: 100%;
   height: 100%;
@@ -10,10 +11,20 @@ export const LoginDiv = styled.div`
   align-items: center;
   height: 100vh;
   width: 100vw;
-  background-image: url(${backgroundImageImac});
+
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
+  @media only screen and (min-width: 1280px) {
+    background-image: url(${backgroundImageImac});
+  }
+  @media only screen and (min-width: 768px) {
+    background-image: url(${backgroundImageTablet});
+  }
+
+  @media only screen and (max-width: 767px) {
+    background-image: url(${backgroundImageMobail});
+  }
 `;
 
 export const LoginDivForm = styled.div`
@@ -25,9 +36,7 @@ export const LoginDivForm = styled.div`
 export const LoginFormContainer = styled.form`
   display: flex;
   border-radius: 8px;
-  background: var(--Form-color, rgba(255, 255, 255, 0.1));
-  box-shadow: 0px 4px 60px 0px rgba(0, 0, 0, 0.25);
-  backdrop-filter: blur(50px);
+
   flex-direction: column;
   align-content: center;
   justify-content: center;
@@ -37,6 +46,18 @@ export const LoginFormContainer = styled.form`
   padding-bottom: 80px;
   padding-left: 62px;
   z-index: 2;
+  @media only screen and (min-width: 768px) {
+    background: var(--Form-color, rgba(255, 255, 255, 0.1));
+
+    box-shadow: 0px 4px 60px 0px rgba(0, 0, 0, 0.25);
+    backdrop-filter: blur(50px);
+  }
+  @media only screen and (max-width: 767px) {
+    padding-top: 97px;
+    padding-right: 20px;
+    padding-bottom: 98px;
+    padding-left: 20px;
+  }
 `;
 
 export const LoginDivButton = styled.div`
@@ -48,6 +69,9 @@ export const LoginDivButton = styled.div`
   flex-direction: column;
   gap: 20px;
   margin-top: 12px;
+  @media only screen and (max-width: 767px) {
+    min-width: 280px;
+  }
 `;
 
 export const InputDiv = styled.div`
@@ -56,6 +80,10 @@ export const InputDiv = styled.div`
 `;
 
 export const LoginFormDivGradient = styled.div`
+  @media only screen and (max-width: 767px) {
+    display: none;
+  }
+
   width: 454px;
   border-radius: 454px;
   background: rgba(47, 21, 176, 0.73);
@@ -77,6 +105,10 @@ export const LoginSpanLogo = styled.span`
 `;
 
 export const LoginInput = styled.input`
+  @media only screen and (max-width: 767px) {
+    padding: 8px 8px 8px 40px;
+    min-width: 280px;
+  }
   min-width: 409px;
   padding: 8px 8px 8px 54px;
   color: #fff;
