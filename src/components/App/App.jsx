9a9aa26411b65fc7d581cca0ec4079c3264f1e55
VictorFil67 <victorfilinovych@gmail.com>
2024-01-12 20:48:47 +0200

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,5 @@
 import Layout from 'components/Layout/Layout';
+import { Loader } from 'components/Loader/Loader';
 import LoginForm from 'components/LoginForm/LoginForm';
 import DashboardPage from 'pages/DashboardPage';
 
@@ -8,9 +9,15 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Navigate, Route, Routes } from 'react-router';
 import { refreshThunk } from 'store/Auth/thunk';
+import { useSelector } from 'react-redux';
+
+import { selectIsLoading } from 'store/Auth/selectors';
+// import { isLoadingSelector } from 'store/currency/currencySelector';
 import Global from 'styles/global';
+
 export const App = () => {
   const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
 
   useEffect(() => {
     dispatch(refreshThunk());
@@ -19,15 +26,34 @@ export const App = () => {
   return (
     <>
       <Global />
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<DashboardPage />} />
-          <Route path="/statistics" element={<StatisticsTab />} />
-        </Route>
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/register" element={<RegistrationPage />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<DashboardPage />} />
+            <Route path="/statistics" element={<StatisticsTab />} />
+          </Route>
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegistrationPage />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      )}
     </>
   );
 };
+{
+  /* <Global />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<DashboardPage />} />
+            <Route path="/statistics" element={<StatisticsTab />} />
+          </Route>
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegistrationPage />} />
+        </Routes>
+      )} */
+}
--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { StyledBalance, StyledTitle, StyledAmount } from './Balance.styled';
+import { useSelector } from 'react-redux';
+// import { selectBalance } from '../../store/Auth/selectors';
 
 export const Balance = () => {
+  // const balance = useSelector(selectBalance);
   return (
     <StyledBalance>
       <StyledTitle>Your balance</StyledTitle>
-      <StyledAmount>₴ 99999</StyledAmount>
+      <StyledAmount>
+        {/* {balance ? `₴ ${balance}` : `Sorry, you must log in!`} */}
+      </StyledAmount>
     </StyledBalance>
   );
 };
