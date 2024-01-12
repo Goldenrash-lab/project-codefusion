500ba603299da92f0f0d29fe4f68707f145c15f9
VictorFil67 <victorfilinovych@gmail.com>
2024-01-12 20:49:13 +0200

    update

--- src/store/Auth/selectors.js
+++ src/store/Auth/selectors.js
@@ -1,3 +1,4 @@
 export const selectBalance = state => state.auth.user.balance;
+// export const selectBalance = state => state.auth.user.balance;
 export const selectUser = state => state.auth.user;
 export const selectIsLoading = state => state.auth.isLoading;
