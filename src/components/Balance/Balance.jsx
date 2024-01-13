de0a988be9c4564c50bf8fbc65a189bbc05dc2d3
Yehor Hryhortsev <egor.goldenrash@gmail.com>
2024-01-13 20:23:39 +0200

    fix warning

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import {
-  StyledBalance,
-  StyledTitle,
-  StyledAmount,
-  StyledBalanceDiv,
-  StyledBalanceDivText,
-} from './Balance.styled';
+import { StyledBalance, StyledTitle, StyledAmount } from './Balance.styled';
 import { useSelector } from 'react-redux';
 import { selectBalance } from 'store/Auth/selectors';
 import Babki from 'images/Babki';
@@ -19,6 +13,5 @@ export const Balance = () => {
         <Babki fill="white" width="26" height="26" /> {balance && balance}
       </StyledAmount>
     </StyledBalance>
-
   );
 };
