8f787d50e80fea595d2a254f9feaa1f0f80b2554
Daryna8 <142911382+Daryna8@users.noreply.github.com>
2024-01-13 20:29:20 +0200

    selectors styles done

--- src/components/StatisticsDashboard/StatisticsDashboard.styled.js
+++ src/components/StatisticsDashboard/StatisticsDashboard.styled.js
@@ -19,25 +19,3 @@ export const StyledContainer = styled.div`
     gap: 20px;
   }
 `;
-
-export const StyledSelect = styled.select`
-  display: flex;
-  align-items: center;
-  font-family: 'Poppins-Regular';
-  color: white;
-  font-size: 12px;
-  width: 182px;
-  height: 50px;
-  padding: 13px 20px;
-  border-radius: 8px;
-  border: 1px solid var(--white-60, rgba(255, 255, 255, 0.6));
-  background: rgba(74, 86, 226, 0.1);
-
-  @media only screen and (min-width: 768px) {
-    width: 160px;
-  }
-
-  @media only screen and (max-width: 767px) {
-    width: 280px;
-  }
-`;
